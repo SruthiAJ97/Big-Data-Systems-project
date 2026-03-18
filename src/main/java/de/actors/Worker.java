ca5882ca8961df7d690e88eef8893aa8a180861e
SruthiAJ97 <sruthijoseph0797@gmail.com>
2026-03-18 13:08:49 +0100

    file upload

--- src/main/java/de/actors/Worker.java
+++ src/main/java/de/actors/Worker.java
@@ -9,6 +9,7 @@ import akka.actor.typed.javadsl.Behaviors;
 import akka.actor.typed.javadsl.Receive;
 import de.ddm.actors.patterns.Reaper;
 import de.ddm.actors.profiling.DependencyWorker;
+import de.ddm.actors.profiling.DataProvider;
 import de.ddm.serialization.AkkaSerializable;
 import de.ddm.singletons.SystemConfigurationSingleton;
 import lombok.NoArgsConstructor;
@@ -18,64 +19,68 @@ import java.util.List;
 
 public class Worker extends AbstractBehavior<Worker.Message> {
 
-	////////////////////
-	// Actor Messages //
-	////////////////////
+    ////////////////////
+    // Actor Messages //
+    ////////////////////
 
-	public interface Message extends AkkaSerializable {
-	}
+    public interface Message extends AkkaSerializable {}
 
-	@NoArgsConstructor
-	public static class ShutdownMessage implements Message {
-		private static final long serialVersionUID = 7516129288777469221L;
-	}
+    @NoArgsConstructor
+    public static class ShutdownMessage implements Message {
+        private static final long serialVersionUID = 7516129288777469221L;
+    }
 
-	////////////////////////
-	// Actor Construction //
-	////////////////////////
+    ////////////////////////
+    // Actor Construction //
+    ////////////////////////
 
-	public static final String DEFAULT_NAME = "worker";
+    public static final String DEFAULT_NAME = "worker";
 
-	public static Behavior<Message> create() {
-		return Behaviors.setup(Worker::new);
-	}
+    public static Behavior<Message> create() {
+        return Behaviors.setup(Worker::new);
+    }
 
-	private Worker(ActorContext<Message> context) {
-		super(context);
-		Reaper.watchWithDefaultReaper(this.getContext().getSelf());
+    ///////////////////
+    // Actor State   //
+    ///////////////////
 
-		final int numWorkers = SystemConfigurationSingleton.get().getNumWorkers();
+    private final List<ActorRef<DependencyWorker.Message>> workers;
 
-		this.workers = new ArrayList<>(numWorkers);
-		for (int id = 0; id < numWorkers; id++)
-			this.workers.add(context.spawn(
-					DependencyWorker.create(),
-					DependencyWorker.DEFAULT_NAME + "_" + id,
-					DispatcherSelector.fromConfig("akka.worker-pool-dispatcher")));
-	}
+    private Worker(ActorContext<Message> context) {
+        super(context);
+        Reaper.watchWithDefaultReaper(this.getContext().getSelf());
 
-	/////////////////
-	// Actor State //
-	/////////////////
+        final int numOfWorkers = SystemConfigurationSingleton.get().getNumWorkers();
 
-	final List<ActorRef<DependencyWorker.Message>> workers;
+        this.workers = new ArrayList<>(Math.max(numOfWorkers, 0));
 
-	////////////////////
-	// Actor Behavior //
-	////////////////////
+        if (numOfWorkers <= 0) {
+            context.getLog().warn("Workers not configured, so system will not process data");
+            return;
+        }
+        ActorRef<DataProvider.Message> dataProvider =
+                context.spawn(DataProvider.create(), DataProvider.DEFAULT_NAME);
 
-	@Override
-	public Receive<Message> createReceive() {
-		return newReceiveBuilder()
-				.onMessage(ShutdownMessage.class, this::handle)
-				.build();
-	}
+        for (int i = 0; i < numOfWorkers; i++) {
+            this.workers.add(context.spawn(
+                    DependencyWorker.create(dataProvider),
+                    DependencyWorker.DEFAULT_NAME + "_" + i,
+                    DispatcherSelector.fromConfig("akka.worker-pool-dispatcher")));
+        }
+    }
 
-	private Behavior<Message> handle(ShutdownMessage message) {
-		// If we expect the system to still be active when the a ShutdownMessage is issued,
-		// we should propagate this ShutdownMessage to all active child actors so that they
-		// can end their protocols in a clean way. Simply stopping this actor also stops all
-		// child actors, but in a hard way!
-		return Behaviors.stopped();
-	}
+    //////////////////////
+    // Actor Behavior   //
+    //////////////////////
+
+    @Override
+    public Receive<Message> createReceive() {
+        return newReceiveBuilder()
+                .onMessage(ShutdownMessage.class, this::handle)
+                .build();
+    }
+
+    private Behavior<Message> handle(ShutdownMessage message) {
+        return Behaviors.stopped();
+    }
 }
